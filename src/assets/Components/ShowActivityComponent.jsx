3899aa2c6111261158858c9392e4e2ee9531c49e
matiibuffaRC <matbuffautn05@gmail.com>
2026-04-23 18:59:07 -0300

    Mejoras en las actividades del homePage

--- src/assets/Components/ShowActivityComponent.jsx
+++ src/assets/Components/ShowActivityComponent.jsx
@@ -3,21 +3,26 @@ import { Link } from 'react-router-dom'
 
 function ShowActivityComponent({ movements }) {
     const printMovements = () => {
-        return [...movements].reverse().map(mov => (
-            <div key={mov.id} className='bg-[#FEFEFE] flex flex-row justify-between items-start p-3 rounded-xl shadow'>
-                <h2 className='text-md font-bold'>{mov.descripcion}</h2>
-                <div className='flex flex-col items-end'>
-                    <h3 className={`font-bold text-md ${mov.tipo === "Ingreso" ? "text-[#00A650]" : ""}`}>{mov.tipo === "Ingreso" ? "+" : "-"}${mov.monto.toFixed(2)}</h3>
-                    <h3 className="text-sm text-gray-500">
-                        {new Date(mov.fecha).toLocaleDateString("es-AR", {
-                            day: "2-digit",
-                            month: "2-digit"
-                        })}
-                    </h3>
+        return [...movements]
+            .reverse()
+            .slice(0, 3)
+            .map(mov => (
+                <div key={mov.id} className='bg-[#FEFEFE] flex flex-row justify-between items-start p-3 rounded-xl shadow'>
+                    <h2 className='text-md font-bold'>{mov.descripcion}</h2>
+                    <div className='flex flex-col items-end'>
+                        <h3 className={`font-bold text-md ${mov.tipo === "Ingreso" ? "text-[#00A650]" : ""}`}>
+                            {mov.tipo === "Ingreso" ? "+" : "-"}${mov.monto.toFixed(2)}
+                        </h3>
+                        <h3 className="text-sm text-gray-500">
+                            {new Date(mov.fecha).toLocaleDateString("es-AR", {
+                                day: "2-digit",
+                                month: "2-digit"
+                            })}
+                        </h3>
+                    </div>
                 </div>
-            </div>
-        )
-    )}
+            ))
+    }
     
     return (
         <div>
@@ -27,7 +32,15 @@ function ShowActivityComponent({ movements }) {
             </div>
             <div className='flex flex-col gap-2 px-2'>
                 {printMovements()}
+                {movements.length > 3 && (
+                    <div className="flex justify-center mt-2">
+                        <Link to="/activity" className="text-blue-500 text-sm flex items-center gap-1 animate-bounce">
+                            Ver más ↓
+                        </Link>
+                    </div>
+                )}
             </div>
+
         </div>
     )
 }
